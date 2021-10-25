0a32d57863154554a8e8faaf8bd85940a70f5b0e
samilarinc <msarinc@hotmail.com>
2021-10-25 19:11:29 +0300

    deneme

--- HW1/hw1.cpp
+++ HW1/hw1.cpp
@@ -2,6 +2,10 @@
 #include<getopt.h>
 #include<chrono>
 #include<sys/stat.h>
+#include<vector>
+
+#define ALPHABET_LEN 4
+#define MAX(a, b) ((a>b) ?(a) :(b))
 
 using namespace std;
 size_t length = 0;
@@ -75,91 +79,104 @@ int KMP(string pattern, string text)
     int i = 0;
     int j = 0;
     while(i < text_length){
-        comparison_kmp++;
-        if(text[i] == pattern[j]){
-            if(j == pattern_length-1){
-                return i-j;
-            }
-            else{
-                i++;
-                j++;
-            }
-        }
-        else if(j > 0){
-            j = failure[j-1];
-        }
-        else{
+        bool flag = text[i] == pattern[j];
+        comparison_kmp += 1;
+        if(flag){
+            j++;
             i++;
-            j = 0;
+        }
+        if(j == pattern_length){
+            return i - j;
+        }
+        else if((i < text_length) && !flag){
+            if(j != 0){
+                j = failure[j - 1];
+            }
+            else
+                i++;
         }
     }
     return -1;
 }
 
-void good_suffix_1(int *shift, int *bpos, string pattern)
+int* bad_char(string pattern)
 {
-    size_t pattern_length = pattern.length();
-    int i = pattern_length;
-    int j = pattern_length + 1;
-    bpos[i] = j;
-  
-    while(i > 0){
-        while(j <= pattern_length && pattern[i-1] != pattern[j-1]){
-            if (shift[j] == 0)
-                shift[j] = j - i;
-            j = bpos[j];
-        }
-        i--;
-        j--;
-        bpos[i] = j; 
+    int i, len = pattern.length();
+    uint8_t letter;
+    int* badchar = new int[ALPHABET_LEN];
+    for (i = 0; i < ALPHABET_LEN; i++)
+        badchar[i] = -1;
+    for (i = 0; i < len; i++){
+        letter = (uint8_t) pattern[i];
+        letter = letter>>1;
+        letter = letter & 0b00000011;
+        badchar[letter] = i-1;
     }
+    return badchar;
 }
 
-void good_suffix_2(int *shift, int *bpos, string pattern)
+int* good_suffix(string pattern)
 {
-    size_t pattern_length = pattern.length();
-    int i, j;
-    j = bpos[0];
-    for(i = 0; i <= pattern_length; i++){
-        if(shift[i] == 0)
-            shift[i] = j;
-        if (i == j)
-            j = bpos[j];
-    }
-}
+	size_t len = pattern.length();
+    int i = len;
+    int j = len + 1;
+    // vector<int> bpos(len+1), shifts(len+1,0); 
+	int* bpos = new int[len+1];
+    int* shifts = new int[len+1];
+    for(int k = 0; k <= len; k++){
+        shifts[k] = 0;
+    } 
+	bpos[i]= j;
+	while(i> 0)  
+	{
+		while( j<=len && pattern[i-1]!= pattern[j-1])
+		{
+			if(shifts[j]==0)
+				shifts[j] = j-i;
+			j = bpos[j]; 
+		}
+		i--;
+		j--;
+		bpos[i] = j;
+	}
+	j = bpos[0];
+	for(i=0; i<=len; i++)
+	{
+		if(shifts[i] == 0)
+			shifts[i] = j;
+		if(i == j)
+			j = bpos[j];
+	}
+	return shifts;
+}	
 
 int BM(string pattern, string text)
 {
-    int s=0, j;
+    int i = 0;
+    int j;
+    comparison_bm = 0;
     int pattern_length = pattern.length();
     int text_length = text.length();
-    comparison_bm = 0;
-  
-    int bpos[pattern_length+1], shift[pattern_length+1];
-  
-    for(int i = 0; i < pattern_length+1; i++) 
-        shift[i] = 0;
-  
-    good_suffix_1(shift, bpos, pattern);
-    for(int i = 0; i < pattern_length+1; i++){
-        cout<<shift[i]<<" ";
-    }cout<<endl;
-    good_suffix_2(shift, bpos, pattern);
-    for(int i = 0; i < pattern_length+1; i++){
-        cout<<shift[i]<<" ";
-    }cout<<endl;
-  
-    while(s <= text_length-pattern_length){
-        j = pattern_length-1;
-        while(j >= 0 && pattern[j] == text[s+j] && ++comparison_bm){
+    int badShift, goodShift;
+    int* badChars = bad_char(pattern);
+    int* shifts = good_suffix(pattern);
+    shifts[0] = 0;
+    while(i <= text_length - pattern_length){
+        j = pattern_length - 1;
+        while(j >= 0 && ++comparison_bm && pattern[j] == text[i+j]){
             j--;
         }
-        if (j < 0)
-        {
-            return s;
+        if(j < 0){
+            return i;
         }
-        else
-            s += shift[j+1];
+        else{
+            uint8_t letter = (uint8_t) text[i+pattern_length];
+            letter = letter>>1;
+            letter = letter & 0b00000011;
+            badShift = MAX(1, j-badChars[letter]);
+            goodShift = shifts[0];
+            i += MAX(badShift, goodShift);
+        }
     }
     return -1;
 }
@@ -237,18 +254,18 @@ int main(int argc, char * argv[])
         duration<double, milli> runtime_kmp = t3 - t2;
         duration<double, milli> runtime_bm = t4 - t3;
         write_result("Brute Force", index_bf, comparison_bf, runtime_bf.count());
-        write_result("Knuth-Morris-Pratt", index_bf, comparison_kmp, runtime_kmp.count());
-        write_result("Boyer-Moore", index_bf, comparison_bm, runtime_bm.count());
+        write_result("Knuth-Morris-Pratt", index_kmp, comparison_kmp, runtime_kmp.count());
+        write_result("Boyer-Moore", index_bm, comparison_bm, runtime_bm.count());
         printf("The best algorithm was ");
-        if(runtime_bf < runtime_kmp){
-            if(runtime_bf < runtime_bm){
+        if(comparison_bf < comparison_kmp){
+            if(comparison_bf < comparison_bm){
                 printf("Brute Force.");
             }
             else{
                 printf("Boyer-Moore.");
             }
         }
-        else if(runtime_bm < runtime_kmp){
+        else if(comparison_bm < comparison_kmp){
             printf("Boyer-Moore.");
         }
         else{
@@ -257,20 +274,29 @@ int main(int argc, char * argv[])
     }
 
     if(method[0] == 'B' && method[1] == 'F'){
+        auto t1 = timer::now();
         int index_bf = brute_force(pattern, text) + 1;
-        write_result("Brute Force", index_bf, 0, 0);
+        auto t2 = timer::now();
+        duration<double, milli> runtime_bf = t2 - t1;
+        write_result("Brute Force", index_bf, comparison_bf, runtime_bf.count());
     }
 
     if(method[0] == 'K' && method[1] == 'M' && method[2] == 'P'){
+        auto t1 = timer::now();
         int index_kmp = KMP(pattern, text) + 1;
-        write_result("Knuth-Morris-Pratt", index_kmp, 0, 0);
+        auto t2 = timer::now();
+        duration<double, milli> runtime_kmp = t2 - t1;
+        write_result("Knuth-Morris-Pratt", index_kmp, comparison_kmp, runtime_kmp.count());
     }
 
     if(method[0] == 'B' && method[1] == 'M'){
+        auto t1 = timer::now();
         int index_bm = BM(pattern, text) + 1;
-        write_result("Boyer-Moore", index_bm, 0, 0);
+        auto t2 = timer::now();
+        duration<double, milli> runtime_bm = t2 - t1;
+        write_result("Boyer-Moore", index_bm, comparison_bm, runtime_bm.count());
     }
     
-    cout<<endl<<endl;
+    cout<<endl;
     return 0;
 }
